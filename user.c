196397a3fc630e75979962fb888050c2af8f759f
kimpeter2018 <kimpeter2018@gmail.com>
2021-05-10 23:22:04 +0900

    update user.c

--- user.c
+++ user.c
@@ -1,13 +1,16 @@
+#include "game.h"
 #include "user.h"
 
 int addScore(Game *s)
 {
   getchar();
-  printf("What's your name? ");
+  gotoxy(MAP_X + (MAP_WIDTH / 2) - 7, MAP_Y + 5, "<What's your name?> ");
+
   scanf("%[^\n]", s->userName);
 
-  printf("==> ADDED SUCCESSFULLY!\n");
-  printf("<  PRESS ENTER  >");
+  gotoxy(MAP_X + (MAP_WIDTH / 2) - 7, MAP_Y + 6, "<==> Added!>");
+  gotoxy(MAP_X + (MAP_WIDTH / 2) - 7, MAP_Y + 7, "<  PRESS ENTER  >");
+  
   return 1;
 }
 
@@ -22,13 +25,7 @@ int updateScoreBoard(Game *s)
   getchar();
   scanf("%[^\n]", s->userName);
 
+
   printf("==> YOUR NAME IS UPDATED!\n");
   return 1;
-}
-
-int deleteMenu(Game *s)
-{
-  s->score = -1;
-  printf("==> DELETE COMPLETE!\n");
-  return 0;
 }
