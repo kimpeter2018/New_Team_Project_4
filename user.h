ec76992eb347e77cc48dcd9b6f0df9fe94d23fc3
csc7545 <csc7545@naver.com>
2021-05-10 10:49:21 +0900

    선언 수정

--- user.h
+++ user.h
@@ -1,9 +1,24 @@
 #ifndef USER_H
 #define USER_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<windows.h>
+#include<conio.h>
+#include<time.h>
+
+#define LEFT 75
+#define RIGHT 77
+#define UP 72
+#define DOWN 80
+#define PAUSE 112
+#define ESC 27
+
+#define MAP_X 3
+#define MAP_Y 2
+#define MAP_WIDTH 30
+#define MAP_HEIGHT 20
 
 typedef struct {
         char userName[20];
